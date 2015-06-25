7e9d6682629726d43449bb960a331e20d6f664d4
David Ashkas <david@ashkas.com>
2015-06-25 11:35:02 +1000

    Wrapping link around block

--- templates/component-single-bottom-blocks.php
+++ templates/component-single-bottom-blocks.php
@@ -20,38 +20,39 @@
 									
 	foreach( $query as $post ) :	setup_postdata($post); ?>
 		<div class="article_blocks">
-			<?php 				  
-			$thumb_id = get_field('post_thumbnail',$post->ID);
-			$small_thumb = wp_get_attachment_image_src($thumb_id,'featured-small');
-		    $large_thumb = wp_get_attachment_image_src($thumb_id,'featured-medium');
-		   
-	/*
-			if($small_thumb): 
-				$width_check = 300;	
-				list($width, $height) = getimagesize($small_thumb[0]);
-				if ($width_check == $width) {
-					$small_thumb = wp_get_attachment_image_src($thumb_id, 'featured-small');
-					
-				} else {
-					$small_thumb = false;
-				}
-			endif;
-	*/	
-							  
-			if($large_thumb): 
-				$alt = get_post_meta($article->ID, '_wp_attachment_image_alt', true);?>
-				<picture>
-					<?php if($large_thumb): ?>
-						<!--[if IE 9]><video style="display: none;"><![endif]-->
-							<source srcset="<?php echo $large_thumb[0] ?>" media="(min-width: 768px)" alt="<?php echo $alt; ?>">
-						<!--[if IE 9]></video><![endif]-->
-					<?php endif; //large_thumb ?>
-					<img srcset="<?php echo $small_thumb[0]; ?>" alt="<?php echo $alt; ?>">
-				</picture>
-			<?php endif; ?>
-			 
-			<h4><a href="<?php echo get_permalink( $post->ID ); ?>"><?php echo $post->post_title; ?></a></h4>
-			
+			<a href="<?php echo get_permalink( $post->ID ); ?>">
+				<?php 				  
+				$thumb_id = get_field('post_thumbnail',$post->ID);
+				$small_thumb = wp_get_attachment_image_src($thumb_id,'featured-small');
+			    $large_thumb = wp_get_attachment_image_src($thumb_id,'featured-medium');
+			   
+		/*
+				if($small_thumb): 
+					$width_check = 300;	
+					list($width, $height) = getimagesize($small_thumb[0]);
+					if ($width_check == $width) {
+						$small_thumb = wp_get_attachment_image_src($thumb_id, 'featured-small');
+						
+					} else {
+						$small_thumb = false;
+					}
+				endif;
+		*/	
+								  
+				if($large_thumb): 
+					$alt = get_post_meta($article->ID, '_wp_attachment_image_alt', true);?>
+					<picture>
+						<?php if($large_thumb): ?>
+							<!--[if IE 9]><video style="display: none;"><![endif]-->
+								<source srcset="<?php echo $large_thumb[0] ?>" media="(min-width: 768px)" alt="<?php echo $alt; ?>">
+							<!--[if IE 9]></video><![endif]-->
+						<?php endif; //large_thumb ?>
+						<img srcset="<?php echo $small_thumb[0]; ?>" alt="<?php echo $alt; ?>">
+					</picture>
+				<?php endif; ?>
+				 
+				<h4><?php echo $post->post_title; ?></h4>
+			</a>
 		</div><!-- /.snippet -->
 	<?php endforeach; 
 	echo '<div class="clearfix"></div>';
